f5a2774a0e8b4beea112db6cb4d198710d156de2
zod-the-immortal <167548880+rottingratto@users.noreply.github.com>
2025-06-25 21:38:43 -0600

    Add files via upload

---
+++ zods library.bat
@@ -0,0 +1,38 @@
+@echo off >nul
+mode 85, 45
+cd files
+:start 
+chcp 65001 >nul
+
+:banner
+cls
+echo.
+echo.
+echo.
+ping localhost -n 2 >nul
+echo [36m
+echo -------------------------------------------------------------------------------------------------------
+echo        ███████╗ ██████╗ ██████╗ ███████╗    ██╗     ██╗██████╗ ██████╗  █████╗ ██████╗ ██╗   ██╗
+echo        ╚══███╔╝██╔═══██╗██╔══██╗██╔════╝    ██║     ██║██╔══██╗██╔══██╗██╔══██╗██╔══██╗╚██╗ ██╔╝
+echo          ███╔╝ ██║   ██║██║  ██║███████╗    ██║     ██║██████╔╝██████╔╝███████║██████╔╝ ╚████╔╝ 
+echo         ███╔╝  ██║   ██║██║  ██║╚════██║    ██║     ██║██╔══██╗██╔══██╗██╔══██║██╔══██╗  ╚██╔╝  
+echo        ███████╗╚██████╔╝██████╔╝███████║    ███████╗██║██████╔╝██║  ██║██║  ██║██║  ██║   ██║   
+echo        ╚══════╝ ╚═════╝ ╚═════╝ ╚══════╝    ╚══════╝╚═╝╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝   ╚═╝  
+echo                                             by zod-the-immortal         
+echo -------------------------------------------------------------------------------------------------------
+echo.
+echo.
+echo.
+echo ╦═══►  learn how to hack easy here
+echo.║ 
+echo ╚╦═══► https://www.hacksplaining.com/lessons 
+echo. ║
+echo  ║   free website to hack (educatonal)  
+echo. ║  
+echo  ╚╦═══►https://www.hackthissite.org 
+echo.  ║
+echo   ║   ai to help learn anything 
+echo.  ║
+echo   ╚═══► https://maestro.org
+echo.   
+pause.
